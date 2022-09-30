7c01f9228562eb36fffa0d702a98133e3acd2262
Maxim Zhosan <maxim.zhosan@gmail.com>
2022-09-30 09:22:34 +0300

    refactor: 12 ПР: Замечание по ревью 2

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -30,10 +30,10 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
     return (
         <PopupWithForm name="profile" title="Редактировать профиль" isOpen={isOpen} onClose={onClose} buttonText='Сохранить' onSubmit={handleSubmit}>
             <input type="text" id="popup__input_type_name" className="popup__input popup__input_type_name" name="name"
-                placeholder="Имя" required minLength="2" maxLength="40" value={name} onChange={handleChangeName} />
+                placeholder="Имя" required minLength="2" maxLength="40" value={name || ''} onChange={handleChangeName} />
             <span id="popup__input_type_name-error" className="popup__error"></span>
             <input type="text" id="popup__input_type_job" className="popup__input popup__input_type_job" name="about"
-                placeholder="Призвание" required minLength="2" maxLength="200" value={description} onChange={handleChangeDescription} />
+                placeholder="Призвание" required minLength="2" maxLength="200" value={description || ''} onChange={handleChangeDescription} />
             <span id="popup__input_type_job-error" className="popup__error"></span>
         </PopupWithForm>
     )
